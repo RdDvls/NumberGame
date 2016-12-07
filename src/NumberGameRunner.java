1c213cd0518c994dbf3e721f74e4665a08eed4cd
Clayton Strickland <cjstrickland5@gmail.com>
2016-12-07 12:58:54 -0500

    Completed. Added tie-breaker functionality

---
+++ src/NumberGameRunner.java
@@ -0,0 +1,11 @@
+/**
+ * Created by RdDvls on 12/7/16.
+ */
+public class NumberGameRunner {
+
+
+    public static void main(String[] args) throws Exception {
+        GameMenu mainMenu = new GameMenu();
+        mainMenu.gameMenu();
+    }
+}
