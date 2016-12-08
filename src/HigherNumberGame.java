923d98c2082672fe8231f777bb4245d05719fcf6
Clayton Strickland <cjstrickland5@gmail.com>
2016-12-08 11:23:39 -0500

    Adding result storage and result printInfo().

---
+++ src/HigherNumberGame.java
@@ -0,0 +1,98 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by RdDvls on 12/8/16.
+ */
+public class HigherNumberGame {
+
+    public static void clearConsole() {
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    }
+
+    public static int getRandomNumber() {
+        return (int) (Math.random() * 10);
+    }
+    public static void guessHigherNumberGame(Scanner inputScanner, int sleepTime){
+        List <String> resultList = new ArrayList<>();
+        int winCounter;
+        int lossCounter;
+        while (true) {
+            try {
+                System.out.println("Guess a higher number");
+                Thread.sleep(sleepTime);
+                clearConsole();
+                System.out.println("How many rounds would you like to play?");
+                int numberOfRounds = Integer.valueOf(inputScanner.nextLine());
+                if (numberOfRounds == 0) {
+                    break;
+                }
+                for (int roundCounter = 0; roundCounter < numberOfRounds; roundCounter++) {
+                    System.out.println("Round # " + (roundCounter + 1));
+                    System.out.println("Please enter a number between one and ten");
+                    int playerNumberChoice = Integer.valueOf(inputScanner.nextLine());
+                    //Check that input is between one and ten. If not, prompt user to try again.
+                    if (playerNumberChoice < 0 || playerNumberChoice > 10) {
+                        throw new Exception("Number must be between 0 and 10!");
+                    }
+                    System.out.println("You chose: " + playerNumberChoice);
+//                    int computerNumberChoice = getRandomNumber();
+                    int computerNumberChoice = 1;
+                    Thread.sleep(sleepTime);
+                    System.out.println("The computer generated: " + computerNumberChoice);
+                    Thread.sleep(sleepTime);
+                    if (playerNumberChoice > computerNumberChoice) {
+                        System.out.println("Congratulations! You win!\n***********\n");
+                        resultList.add("Round #" + (roundCounter + 1) + ": You chose: " + playerNumberChoice + ", the computer chose: " + computerNumberChoice + " ==> You won");
+                    } else if (playerNumberChoice == computerNumberChoice) {   //Tie scenario
+                        System.out.println("Same numbers were chosen! There must be a winner!\nA coin toss will settle this nonsense!");
+                        int coinFlip = getRandomNumber();
+                        System.out.println("Heads or tails?");
+                        String coinFlipUserChoice = inputScanner.nextLine();
+                        System.out.println("You chose: " + coinFlipUserChoice);
+                        System.out.println("Flipping coin...\n");
+                        Thread.sleep(sleepTime);
+                        if (coinFlipUserChoice.equalsIgnoreCase("heads") && coinFlip <= 5) {
+                            System.out.println("...the coin has landed on heads!");
+                            Thread.sleep(sleepTime);
+                            System.out.println("Fortune favors you. You are the winner.\n************\n");
+                            resultList.add("Round #" + (roundCounter + 1) + ": You chose: " + playerNumberChoice + ", the computer chose: " + computerNumberChoice + " ==> Tiebreaker ==> Coin flip: You chose: " + coinFlipUserChoice + ", the coin landed on " + coinFlip + " ==> You won");
+                        } else if (coinFlipUserChoice.equalsIgnoreCase("heads") && coinFlip > 5) {
+                            System.out.println("...the coin has landed on tails!");
+                            System.out.println("Fortune is not in your favor. You lose.\n************\n");
+                        } else if (coinFlipUserChoice.equalsIgnoreCase("tails") && coinFlip > 5) {
+                            System.out.println("...the coin has landed on tails!");
+                            System.out.println("Fortune favors you. You are the winner.\n************\n");
+                        } else if (coinFlipUserChoice.equalsIgnoreCase("tails") && coinFlip <= 5) {
+                            System.out.println("...the coin has landed on heads!");
+                            System.out.println("Fortune is not in your favor. You lose.\n************\n");
+                        }
+                    } else if (playerNumberChoice < computerNumberChoice) {
+                        System.out.println("You lose.\n************\n");
+                    }
+                }
+                System.out.println("Would you like to play again? Enter y or n");
+                String playAgainChoice = inputScanner.nextLine();
+                if (playAgainChoice.equalsIgnoreCase("y")) {
+                    clearConsole();
+                    GameMenu.gameMenu();
+                } else if (playAgainChoice.equalsIgnoreCase("n")) {
+                    System.out.println("Thank you for playing");
+                    printRoundResults(resultList);
+                }
+                break;
+            } catch (Exception numException) {
+                System.out.println("Number must be between one and ten!");
+            }
+        }
+
+    }
+    public static void printRoundResults(List resultList){
+        System.out.println(resultList.toString());
+    }
+    public static void printTotalResults(List resultList){    /*Finish the round results portion before moving to total results*/
+        System.out.println("Test...");
+    }
+
+}
